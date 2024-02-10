5cca2efe76adf73558f472bc21d3e3001cc482b3
Mihail-21 <139543682+Mihail-21@users.noreply.github.com>
2024-02-10 14:57:43 +0200

    hmwrk

---
+++ js/task-6.js
@@ -0,0 +1,34 @@
+const refs = {
+  amountEl: document.querySelector("#controls input"),
+  createEl: document.querySelector("button[data-create]"),
+  destroyEl: document.querySelector("button[data-destroy]"),
+  boxesEl: document.querySelector("#boxes"),
+};
+refs.createEl.addEventListener("click", onCreateClick);
+refs.destroyEl.addEventListener("click", onDestroyClick);
+function onCreateClick() {
+  let amount = refs.amountEl.value;
+  if (amount < 1 || amount > 100) {
+    return undefined;
+  }
+  onDestroyClick();
+  createBoxes(amount);
+}
+function onDestroyClick() {
+  refs.boxesEl.innerHTML = "";
+  refs.amountEl.value = "";
+}
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    let elem = document.createElement("div");
+    elem.style.width = 30 + 10 * i + "px";
+    elem.style.height = 30 + 10 * i + "px";
+    elem.style.backgroundColor = getRandomHexColor();
+    refs.boxesEl.append(elem);
+  }
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
